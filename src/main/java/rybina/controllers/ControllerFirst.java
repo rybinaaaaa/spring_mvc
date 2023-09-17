293d2a5f2aefe3280d915ca26d1ef1d000c3454c
rybinali <rybinaaaaa.a@gmail.com>
2023-09-17 21:07:23 +0200

    add 2 methods

--- src/main/java/rybina/controllers/ControllerFirst.java
+++ src/main/java/rybina/controllers/ControllerFirst.java
@@ -3,18 +3,26 @@ package rybina.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("first")
 public class ControllerFirst {
 
+    // First Method
     @GetMapping("/hello")
-    public String hello() {
+    public String hello(HttpServletRequest request) {
+        String name = request.getParameter("name");
+        System.out.println("hello " + name);
         return "first/hello";
     }
 
+    // Second method
     @GetMapping("/goodbye")
-    public String goodbye() {
+    public String goodbye(@RequestParam(value = "name", required = false) String name) {
+        System.out.println("hello " + name);
         return "first/goodbye";
     }
 }
