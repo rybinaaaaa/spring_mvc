817891b0ba79c3e3b3f61ca5633af6cbfdff094d
rybinali <rybinaaaaa.a@gmail.com>
2023-09-17 20:54:48 +0200

    fix bugs

--- src/main/java/rybina/controllers/ControllerFirst.java
+++ src/main/java/rybina/controllers/ControllerFirst.java
@@ -1,11 +1,11 @@
-package controllers;
+package rybina.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/first")
+@RequestMapping("first")
 public class ControllerFirst {
 
     @GetMapping("/hello")
